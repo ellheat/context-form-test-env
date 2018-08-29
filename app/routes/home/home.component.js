a3fa572672856394a210f81aa6e6dc028097206a
Patryk Poleszczuk <patryk.poleszczuk@gmail.com>
2018-08-29 19:12:49 +0200

    Add validate.js

--- app/routes/home/home.component.js
+++ app/routes/home/home.component.js
@@ -1,6 +1,7 @@
-import React, { PureComponent } from 'react';
+import React, { PureComponent, Fragment } from 'react';
 import PropTypes from 'prop-types';
 import Helmet from 'react-helmet';
+import validate from 'validate.js';
 import { FormProvider, Field } from 'context-form/src/main'; //eslint-disable-line
 
 import { Container } from './home.styles';
@@ -28,16 +29,54 @@ export class Home extends PureComponent {
     }
   }
 
-  writeSomething = () => console.log('writeSomething');
+  validate = (values) => {
+    const constraints = {
+      simpleField: {
+        presence: {
+          allowEmpty: false,
+        },
+      },
+    };
+
+    return validate(values, constraints);
+  };
+
+  handleChange = input => (e) => {
+    input.onChange(e.target.value);
+  };
+
+  handleSubmit = () => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, 1000);
+    });
+  };
+
+  renderTextField = ({ input, error }) => {
+    return (
+      <Fragment>
+        <input {...input} onChange={this.handleChange(input)} />
+        <p>{error && error.length ? error[0] : null}</p>
+      </Fragment>
+    );
+  };
+
+  renderForm = ({ handleSubmit, isSubmitting }) => {
+    return (
+      <form onSubmit={handleSubmit(this.handleSubmit)}>
+        <Field
+          name="simpleField"
+          component={this.renderTextField}
+        />
+        { isSubmitting ? 'isSubmitting' : null }
+      </form>
+    );
+  };
 
   render() {
     return (
       <Container>
         <Helmet title="Homepage" />
-
-        <FormProvider>
-          <Field name="simpleField" component="input" />
-        </FormProvider>
+        <FormProvider component={this.renderForm} validate={this.validate} />
       </Container>
     );
   }
